564592b1454651bb80ec6792cd950d2e0588dee5
Olivie Bergeron <111806530+OlivTheOlive@users.noreply.github.com>
2024-05-14 23:16:59 -0400

    Added basic table and made a basic layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,77 @@
-import { Grid } from "@mui/material";
-import CustomButton from "./customButton";
+"use client";
+
+//external imports
+import { Grid, Paper } from "@mui/material";
+import { useState } from "react";
+
+//internal imports
+import CustomFileInput from "./components/CustomFileInput";
+import CustomTable from "./components/CustomTable";
 
 export default function Home() {
+  const [data, setData] = useState([]);
+
+  function fileHandling(event) {}
+
   return (
     <Grid
       container
       sx={{
+        flex: 1,
         justifyContent: "center",
         alignContent: "center",
         minHeight: "100vh",
+        width: "100%",
         flexDirection: "column",
       }}
     >
-      <Grid>Add file</Grid>
-      <CustomButton title={"Insert File"}>File</CustomButton>
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+          marginBottom: 3,
+        }}
+      >
+        <Paper
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 100,
+            background: "white",
+            padding: 4,
+            width: "20%",
+          }}
+        >
+          <CustomFileInput title={"Insert File"} onClick={fileHandling}>
+            File
+          </CustomFileInput>
+        </Paper>
+      </Grid>
+
+      <Grid
+        container
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Paper
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "80%",
+          }}
+        >
+          {data.length !== 1 && <CustomTable data={data} />}
+        </Paper>
+      </Grid>
     </Grid>
   );
 }
