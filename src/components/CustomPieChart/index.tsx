c3ed55b4e1a9651f23dcf2cdecc4644daff842a3
Olivie Bergeron <111806530+OlivTheOlive@users.noreply.github.com>
2024-08-04 17:24:15 -0400

    updated code

--- src/components/CustomPieChart/index.tsx
+++ src/components/CustomPieChart/index.tsx
@@ -1,17 +1,31 @@
 import * as React from "react";
 import { PieChart } from "@mui/x-charts/PieChart";
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 
 interface CustomPieProps {
   data: Record<string, any>[];
 }
 
 export default function PieChartComponent({ data }: CustomPieProps) {
+  const [pickedType, setPickedType] = React.useState<string>("TYPE");
+
+  // Handle the change event for the dropdown
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    setPickedType(event.target.value);
+  };
+
   // Use useMemo to memoize the computation of data points
-  // This ensures that the computation is only done when the data prop changes
+  // This ensures that the computation is only done when the data or pickedType changes
   const dataPoints = React.useMemo(() => {
-    // Reduce the data array to count occurrences of each TYPE
+    // Reduce the data array to count occurrences of each picked type
     const counts = data.reduce((acc, item) => {
-      const type = item.TYPE;
+      const type = item[pickedType] || item.TYPE;
       // Increment the count for the current type, initializing to 0 if it doesn't exist
       acc[type] = (acc[type] || 0) + 1;
       return acc;
@@ -23,17 +37,33 @@ export default function PieChartComponent({ data }: CustomPieProps) {
       value,
       label,
     }));
-  }, [data]);
+  }, [data, pickedType]);
 
   return (
-    <PieChart
-      series={[
-        {
-          data: dataPoints,
-        },
-      ]}
-      width={350}
-      height={200}
-    />
+    <div>
+      <FormControl fullWidth>
+        <InputLabel id="type-picker-label">Select Data Type</InputLabel>
+        <Select
+          labelId="type-picker-label"
+          id="type-picker"
+          value={pickedType}
+          label="Select Data Type"
+          onChange={handleChange}
+        >
+          <MenuItem value="TYPE">TYPE</MenuItem>
+          <MenuItem value="COUNTY">COUNTY</MenuItem>
+          <MenuItem value="GROUP">GROUP</MenuItem>
+        </Select>
+      </FormControl>
+      <PieChart
+        series={[
+          {
+            data: dataPoints,
+          },
+        ]}
+        width={350}
+        height={200}
+      />
+    </div>
   );
 }
